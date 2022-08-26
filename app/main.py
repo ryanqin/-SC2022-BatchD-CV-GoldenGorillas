4ae5c370cf28a08a8f1956838b76bc9debb2c746
ryanqin <ryanqin10@gmail.com>
2022-08-26 21:51:43 +0000

    instruction

--- app/main.py
+++ app/main.py
@@ -148,7 +148,7 @@ def files(filename):
 
 if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
-    website_url = 'cocalc7.ai-camp.dev'
+    website_url = 'localhost'
     
     print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
     app.run(host = '0.0.0.0', port=port, debug=True)
